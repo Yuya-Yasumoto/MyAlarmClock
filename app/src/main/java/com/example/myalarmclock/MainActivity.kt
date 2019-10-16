76f8e2d7db833be11522b784ddc24570cdb7e9b7
yuyayasumoto <yuya.yasumoto@jp.ricoh.com>
2019-10-16 11:02:19 +0900

    完成

--- app/src/main/java/com/example/myalarmclock/MainActivity.kt
+++ app/src/main/java/com/example/myalarmclock/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.myalarmclock
 
 import android.app.AlarmManager
+import android.app.KeyguardManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager.LayoutParams.*
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.IllegalArgumentException
@@ -22,14 +24,16 @@ class MainActivity : AppCompatActivity(),
                         TimePickerFragment.OntimeSelectedListener{ //自作
 
     override fun onSelected(year: Int, month: Int, date: Int) {
-        val c = Calendar.getInstance()
+        val c = getInstance()
         c.set(year,month,date)
-        dateText.text = DateFormat.format("yyyy/MM/dd",c)
+        dateText.text = android.text.format.DateFormat.format("yyyy/MM/dd",c)
     }
 
     override fun onSelected(hourOfDay: Int, minute: Int) {
-        timeText = "%1$02d:%2$02d".format(hourOfDay, minute)
+        timeText.text = "%1$02d:%2$02d".format(hourOfDay, minute)
     }
+
+
 
     override fun getUp() {
         //Toast.makeText(this,"起きるがクリックされました",Toast.LENGTH_SHORT)
@@ -51,6 +55,30 @@ class MainActivity : AppCompatActivity(),
         super.onCreate(savedInstanceState)
 
         if(intent?.getBooleanExtra(ON_RECEIVE,false) == true){
+            when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1 -> {
+                    setShowWhenLocked(true)
+                    setTurnScreenOn(true)
+                    val keyguardManager =
+                        getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+                    keyguardManager.requestDismissKeyguard(this, null)
+                }
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
+                    window.addFlags(
+                        FLAG_TURN_SCREEN_ON or FLAG_SHOW_WHEN_LOCKED
+                    )
+                    val keyguardManager = getSystemService(KEYGUARD_SERVICE) as KeyguardManager
+                    keyguardManager.requestDismissKeyguard(this,null)
+                }
+                else -> {
+                    window.addFlags(
+                        FLAG_TURN_SCREEN_ON or FLAG_SHOW_WHEN_LOCKED or FLAG_DISMISS_KEYGUARD
+                    )
+                }
+            }
+
+
+
             val dialog = TimeAlertDialog()
             dialog.show(supportFragmentManager,"alert_dialog")
         }
